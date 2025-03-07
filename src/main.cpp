9330615c1c1193d81b018c7cb27d10150dd2aad2
Egorkkk <ad.rasao@gmail.com>
2025-03-07 20:05:29 +0300

    Update WiFi SSID

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,15 @@ static AsyncWebServer server(80);
 
 #define FORMAT_LITTLEFS_IF_FAILED true
 
+const char* ssid = "Robocast2.4G";
+const char* password = "1357924680";
+IPAddress local_IP(192, 168, 0, 200);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 0, 1);
 
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
   Serial.printf("Listing directory: %s\r\n", dirname);
@@ -55,8 +63,26 @@ void createDir(fs::FS &fs, const char * path){
 void setup() {
   Serial.begin(115200);
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP("esp-captive");
+  //WiFi.mode(WIFI_AP);
+  //WiFi.softAP("esp-captive");
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+    Serial.println("STA Failed to configure");
+  }
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
 
   if(!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
       Serial.println("LittleFS Mount Failed");
