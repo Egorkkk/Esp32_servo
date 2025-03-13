56a0861952ccf62e5883c6c892de8370def21ae4
Egorkkk <ad.rasao@gmail.com>
2025-03-14 00:14:19 +0300

    Merge with work

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,55 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 
+
+//////////////////////////////////////////////////////////////////////////////////OLED//////////////////////
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_MOSI   22
+#define OLED_CLK   23
+#define OLED_DC    19
+#define OLED_CS    18
+#define OLED_RESET 21
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
+  OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+  #define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+  #define LOGO_HEIGHT   16
+  #define LOGO_WIDTH    16
+  static const unsigned char PROGMEM logo_bmp[] =
+  { 0b00000000, 0b11000000,
+    0b00000001, 0b11000000,
+    0b00000001, 0b11000000,
+    0b00000011, 0b11100000,
+    0b11110011, 0b11100000,
+    0b11111110, 0b11111000,
+    0b01111110, 0b11111111,
+    0b00110011, 0b10011111,
+    0b00011111, 0b11111100,
+    0b00001101, 0b01110000,
+    0b00011011, 0b10100000,
+    0b00111111, 0b11100000,
+    0b00111111, 0b11110000,
+    0b01111100, 0b11110000,
+    0b01110000, 0b01110000,
+    0b00000000, 0b00110000 };
+//////////////////////////////////////////////////////////////////////////////////OLED//////////////////////
+
+
+
+
 static AsyncWebServer server(80);
 
 #define FORMAT_LITTLEFS_IF_FAILED true
 
 const char* ssid = "Robocast2.4G";
-const char* password = "1357924680";
+const char* password = "1008121982";
 IPAddress local_IP(192, 168, 0, 200);
 // Set your Gateway IP address
 IPAddress gateway(192, 168, 0, 1);
@@ -60,14 +103,99 @@ void createDir(fs::FS &fs, const char * path){
 }
 
 
+//////////////////////////////////////////////////////////////////////////////////OLED//////////////////////
+void testdrawchar(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  for(int16_t i=0; i<256; i++) {
+    if(i == '\n') display.write(' ');
+    else          display.write(i);
+  }
+
+  display.display();
+  delay(2000);
+}
+
+#define XPOS   0 // Indexes into the 'icons' array in function below
+#define YPOS   1
+#define DELTAY 2
+
+void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  int8_t f, icons[NUMFLAKES][3];
+
+  // Initialize 'snowflake' positions
+  for(f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+    icons[f][YPOS]   = -LOGO_HEIGHT;
+    icons[f][DELTAY] = random(1, 6);
+    Serial.print(F("x: "));
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(F(" y: "));
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(F(" dy: "));
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  for(;;) { // Loop forever...
+    display.clearDisplay(); // Clear the display buffer
+
+    // Draw each snowflake:
+    for(f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
+    }
+
+    display.display(); // Show the display buffer on the screen
+    delay(200);        // Pause for 1/10 second
+
+    // Then update coordinates of each flake...
+    for(f=0; f< NUMFLAKES; f++) {
+      icons[f][YPOS] += icons[f][DELTAY];
+      // If snowflake is off the bottom of the screen...
+      if (icons[f][YPOS] >= display.height()) {
+        // Reinitialize to a random position, just off the top
+        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+        icons[f][YPOS]   = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
+      }
+    }
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////OLED//////////////////////
+
+
 void setup() {
   Serial.begin(115200);
 
+
+//////////////////////////////////////////////////////////////////////////////////OLED//////////////////////
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  testdrawchar(); 
+  delay(2000);
+  display.clearDisplay();
+  testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT);
+//////////////////////////////////////////////////////////////////////////////////OLED//////////////////////
+
+
+
   //WiFi.mode(WIFI_AP);
   //WiFi.softAP("esp-captive");
-  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-    Serial.println("STA Failed to configure");
-  }
+  //if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+  //  Serial.println("STA Failed to configure");
+  //}
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
